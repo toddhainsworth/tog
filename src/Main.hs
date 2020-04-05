af3ad380cdb42baa9297aa41656d45d9f35f96ec
Todd Hainsworth <hainsworth.todd@gmail.com>
2020-04-05 12:18:06 +0930

    load config from a path and decode it

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,36 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 module Main where
 
+import qualified Data.ByteString.Lazy as B
+import GHC.Generics
+import System.Directory
+import System.FilePath
+import Data.Aeson
+
+-- GOALS
+-- Read JSON file
+-- Parse JSON
+-- Get token from JSON
+-- Push token into Hoggl
+-- Take workspaces and display
+data Config = Config { token :: String } deriving (Show, Generic)
+instance FromJSON Config
+
+configPath :: IO FilePath
+configPath = do
+  homeDir <- getHomeDirectory
+  return $ homeDir </> ".tog"
+
+loadConfig :: IO Config
+loadConfig = do
+  pth <- configPath
+  cf <- configPath >>= B.readFile
+  let config = case (decode cf :: Maybe Config) of
+            Just a -> a
+            Nothing -> Config {}
+  return config
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  config <- loadConfig
+  putStrLn $ show config
