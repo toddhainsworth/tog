9fee8b61ecce99cb6815eea4dc5839ed4200a22f
Todd Hainsworth <hainsworth.todd@gmail.com>
2020-04-06 19:35:40 +0930

    help and stop

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 module Main where
 
+import Control.Monad
+import Data.Text as T
 import qualified Data.ByteString.Lazy as B
 import GHC.Generics
 import System.Console.GetOpt
@@ -9,12 +11,14 @@ import System.Environment
 import System.Exit
 import System.FilePath
 import Data.Aeson
+import Network.Hoggl
+import Network.Hoggl.Types
 
 -- GOALS
 -- Push token into Hoggl
 -- Take workspaces and display
 
-data Flag = Help | Start String | Stop | List deriving (Show)
+data Flag = Help | Start String | Stop | List deriving (Show, Eq)
 
 data Config = Config { token :: String } deriving (Show, Generic)
 instance FromJSON Config
@@ -51,11 +55,29 @@ loadConfig = do
             Nothing -> defaultConfig
   return config
 
+runFlag :: Token -> Flag -> IO ()
+runFlag token Help = putStrLn $ usageInfo "USAGE:" options
+
+runFlag token Stop = do
+  tryStopRunning token
+  return ()
+
+runFlag token List = undefined
+
+newTimeEntry :: String -> TimeEntryStart
+newTimeEntry desc = TES {
+                      tesDescription = Just(T.pack desc),
+                      tesTags = [],
+                      tesPid = Nothing,
+                      tesCreatedWith = "Tog by Todd"
+                                   }
+
 main :: IO ()
 main = do
   cmdArgs <- getArgs
   (flags, args, _) <- return $ getOpt RequireOrder options cmdArgs
-  putStrLn $ show flags
-  putStrLn $ show args
---   config <- loadConfig
---   putStrLn $ show config
+
+  config <- loadConfig
+  let runner = runFlag $ Api(token config)
+  mapM runner flags
+  exitSuccess
