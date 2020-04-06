4d4603d02a59f3bb1675166c4adc365584bc3f19
Todd Hainsworth <hainsworth.todd@gmail.com>
2020-04-06 14:05:55 +0930

    accept command line args

--- src/Main.hs
+++ src/Main.hs
@@ -3,18 +3,40 @@ module Main where
 
 import qualified Data.ByteString.Lazy as B
 import GHC.Generics
+import System.Console.GetOpt
 import System.Directory
+import System.Environment
+import System.Exit
 import System.FilePath
 import Data.Aeson
 
 -- GOALS
 -- Push token into Hoggl
 -- Take workspaces and display
+
+data Flag = Help | Start String | Stop | List deriving (Show)
+
 data Config = Config { token :: String } deriving (Show, Generic)
 instance FromJSON Config
 
 defaultConfig :: Config
 defaultConfig = Config { token = "" }
+
+options :: [OptDescr Flag]
+options = [
+            Option "h" ["help"]
+              (NoArg Help)
+              "display this message",
+            Option "s" ["start"]
+              (ReqArg Start "DESCRIPTION")
+              "start a new timer",
+            Option "S" ["stop"]
+              (NoArg Stop)
+              "stops the current timer",
+            Option "l" ["list"]
+              (NoArg List)
+              "lists timers for the day"
+          ]
 
 configPath :: IO FilePath
 configPath = do
@@ -31,5 +53,9 @@ loadConfig = do
 
 main :: IO ()
 main = do
-  config <- loadConfig
-  putStrLn $ show config
+  cmdArgs <- getArgs
+  (flags, args, _) <- return $ getOpt RequireOrder options cmdArgs
+  putStrLn $ show flags
+  putStrLn $ show args
+--   config <- loadConfig
+--   putStrLn $ show config
